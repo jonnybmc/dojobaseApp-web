9b3f770d1552a28a8c7322311913ddfac5ecc251
Jonathan Booysen <jonnybmc@gmail.com>
2017-12-24 02:49:01 +0200

    changed endpoint calls to https

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService{
         const headers = new Headers({
             'Content-Type' : 'application/json'
         });
-        return this.http.post('http://dojobase-deployment.herokuapp.com/user', body, {headers:headers})
+        return this.http.post('https://dojobase-deployment.herokuapp.com/user', body, {headers:headers})
             .map((response:Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
@@ -28,7 +28,7 @@ export class AuthService{
     signin(user: User) {
         const body = JSON.stringify(user);
         const headers = new Headers({'Content-Type' : 'application/json'});
-        return this.http.post('http://dojobase-deployment.herokuapp.com/user/signin', body, {headers : headers})
+        return this.http.post('https://dojobase-deployment.herokuapp.com/user/signin', body, {headers : headers})
         .map((response:Response) => response.json())
         .catch((error: Response) => {
             this.errorService.handleError(error.json());
